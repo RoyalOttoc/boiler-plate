e8de332c1031d3254cb756391b318744df6296b8
RoyalOttoc <jong.kim8706@gmail.com>
2021-03-21 09:46:11 +1100

    generate password token

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { User } = require('./models/User');
 const config = require('./config/key');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose
@@ -23,13 +25,44 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post('/api/users/login', (req, res) => {
+  // find email in DB
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: 'Not registered Email',
+      });
+    }
+    // compare password
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: 'Password is not correct',
+        });
+      // create token
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // save token in cookie
+        res
+          .cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
