1dc4176ef300fd1879fc1ce02fab0bc6e975f9c9
RoyalOttoc <jong.kim8706@gmail.com>
2021-03-24 02:31:17 +1100

    add logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,25 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello').then((response) => console.log(response));
   }, []);
 
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then((response) => {
+      if (response.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('failed to log out');
+      }
+    });
+  };
   return (
     <div
       style={{
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
@@ -17,6 +27,8 @@ function LandingPage() {
       }}
     >
       <h2>Landng Page</h2>
+
+      <button onClick={onClickHandler}>Log out</button>
     </div>
   );
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function RegisterPage() {
-  return <div>RegisterPage</div>;
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../../_actions/user_action';
+
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState('');
+  const [Name, setName] = useState('');
+  const [Password, setPassword] = useState('');
+  const [ConfirmPassword, setConfirmPassword] = useState('');
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (Password !== ConfirmPassword) {
+      return alert('Password should be match');
+    }
+
+    let body = {
+      email: Email,
+      password: Password,
+      name: Name,
+    };
+
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        props.history.push('/login');
+      } else {
+        alert('Failed to sign up');
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+        onSubmit={onSubmitHandler}
+      >
+        <label htmlFor="">Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+
+        <label htmlFor="">Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+
+        <label htmlFor="">Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+
+        <label htmlFor="">Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+
+        <br />
+        <button>Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
